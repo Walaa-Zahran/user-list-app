76da09414f57f5ce96ec0101716b3ba24327f5c3
walaazahran <walaazahran40@gmail.com>
2024-04-03 02:38:16 +0200

    remove user card

--- src/app/modules/user-list/user-list.module.ts
+++ src/app/modules/user-list/user-list.module.ts
@@ -4,11 +4,10 @@ import { UserListComponent } from './components/user-list/user-list.component';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from '../../core/core.module';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
-import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
-  declarations: [UserListComponent, UserDetailComponent, UserCardComponent],
+  declarations: [UserListComponent, UserDetailComponent],
   imports: [CommonModule, RouterModule, CoreModule],
-  exports: [UserListComponent, UserDetailComponent, UserCardComponent],
+  exports: [UserListComponent, UserDetailComponent],
 })
 export class UserListModule {}
